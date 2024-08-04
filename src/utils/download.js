b6607410786dc897ae0f1165691aeb49a0e3b851
Arthur Poiret <contact@arthurpoiret.fr>
2024-08-04 10:48:44 +0200

    fix: add user-agent to download request

--- src/utils/download.js
+++ src/utils/download.js
@@ -1,17 +1,21 @@
 const DownloaderHelper = require("node-downloader-helper");
 
 module.exports.download = (url, saveDirectory) => {
-  const downloader = new DownloaderHelper.DownloaderHelper(url, saveDirectory);
-  return new Promise(
-      (resolve, reject) => {
-          downloader.on("end", () =>resolve(downloader.getDownloadPath()));
-          downloader.on("error", (error) => reject(error));
-          downloader.on("progress.throttled", (downloadEvents) => {
-              const percentageComplete = downloadEvents.progress < 100 ? downloadEvents.progress.toPrecision(2) : 100;
-              console.log(`Downloaded: ${percentageComplete}%`)
-          });
-          downloader.start();
-      }
-  );
+    const downloader = new DownloaderHelper.DownloaderHelper(url, saveDirectory, {
+        headers: {
+            'User-Agent': 'owlplug/node',
+        }
+    });
+    return new Promise(
+        (resolve, reject) => {
+            downloader.on("end", () => resolve(downloader.getDownloadPath()));
+            downloader.on("error", (error) => reject(error));
+            downloader.on("progress.throttled", (downloadEvents) => {
+                const percentageComplete = downloadEvents.progress < 100 ? downloadEvents.progress.toPrecision(2) : 100;
+                console.log(`Downloaded: ${percentageComplete}%`)
+            });
+            downloader.start();
+        }
+    );
 
 }
